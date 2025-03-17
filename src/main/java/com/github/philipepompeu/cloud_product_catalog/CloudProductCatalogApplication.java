aeea007d8462143d06f63ff83c0eb05ec84198af
philipepompeu <philipepompeu2@gmail.com>
2025-03-17 17:57:23 -0300

    commit inicial da criação da API

---
+++ src/main/java/com/github/philipepompeu/cloud_product_catalog/CloudProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.github.philipepompeu.cloud_product_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudProductCatalogApplication.class, args);
+	}
+
+}
